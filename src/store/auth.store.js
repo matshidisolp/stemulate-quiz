e18c1b05103b7503ebe1a30570f61e8e0b581228
matshidisolp <mpitswane@gmail.com>
2025-08-20 06:48:53 +0700

    login and signup store

--- src/store/auth.store.js
+++ src/store/auth.store.js
@@ -1,1 +1,38 @@
-i
+import { create } from 'zustand';
+import { loginUser, SignUpUser } from '../services/auth.service';
+
+export const useAuthStore = create((set) => ({
+    // Initial state
+    user: null,
+    isAuthenticated: false,
+    isLoading: false,
+    error: null,
+
+    // Actions
+    login: async (email, password) => {
+        set({ isLoading: true, error: null});
+        try {
+            const user=await loginUser(email, password);
+            set({ user:user, isAuthenticated: true, isLoading: false});
+        } catch (error) {
+            set({ error: error.message, isLoading: false });
+            throw error;
+        }
+    },
+
+    logout: () => {
+        // Firebase sign-out logic 
+        set({ user: null, isAuthenticated: false });
+    },
+
+    signup: async (email, password) => {
+        set({ isLoading: true, error: null });
+        try {
+            const user = await SignUpUser(email, password);
+            set({ user: user, isAuthenticated: true, isLoading: false });
+        } catch (error) {
+            set({ error: error.message, isLoading: false });
+            throw error;
+        }
+    },
+}));
